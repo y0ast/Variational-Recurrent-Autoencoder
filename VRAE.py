fb0b0ed1fb689fced9c0f5e00144932a8f53a2a9
Joost van Amersfoort <joost.van.amersfoort@gmail.com>
2015-08-03 10:42:03 +0200

    Fix pickle

--- VRAE.py
+++ VRAE.py
@@ -203,8 +203,8 @@ class VRAE:
     def load_parameters(self, path):
         """Load the variables in a shared variable safe way"""
         p_list = pickle.load(open(path + "/params.pkl", "rb"))
-        m_list = cPickle.load(open(path + "/m.pkl", "rb"))
-        v_list = cPickle.load(open(path + "/v.pkl", "rb"))
+        m_list = pickle.load(open(path + "/m.pkl", "rb"))
+        v_list = pickle.load(open(path + "/v.pkl", "rb"))
 
         for name in p_list.keys():
             self.p[name].set_value(p_list[name].astype(theano.config.floatX))
